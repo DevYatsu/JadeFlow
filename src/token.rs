785baa6b242053073aeac1fb75f31e8d48d1faaf
IronYatsu <yatsu.dev@gmail.com>
2023-07-12 00:44:20 +0200

    line and block comments implemented with # and ### respectively, numbers lexing right as well as assignement and binary operators +,-,*,/,% and =

--- src/token.rs
+++ src/token.rs
@@ -39,6 +39,7 @@ pub fn tokenize(source_code: &str) -> Result<Vec<Token>, String> {
     let mut position = 0;
 
     while position < source_code.len() {
+        println!("position {position}");
         let character: char = source_code.chars().nth(position).unwrap();
         println!("{}", character);
 
@@ -53,33 +54,36 @@ pub fn tokenize(source_code: &str) -> Result<Vec<Token>, String> {
                 } else {
                     tokens.push(token(&operator_lexeme, TokenType::BinaryOperator));
                 }
-            },
+                println!("{operator_lexeme}");
+            }
             '=' => {
                 let mut equal_lexeme = character.to_string();
 
                 if source_code.as_bytes().get(position + 1) == Some(&(b'=' as u8)) {
-                        position += 1;
-                        equal_lexeme.push('=');
-                        tokens.push(token(&equal_lexeme, TokenType::ComparisonOperator));
+                    position += 1;
+                    equal_lexeme.push('=');
+                    tokens.push(token(&equal_lexeme, TokenType::ComparisonOperator));
                 } else {
                     tokens.push(token(&equal_lexeme, TokenType::AssignmentOperator));
                 }
-            },
+            }
             c if character.is_digit(10) => {
                 let mut number_lexeme = c.to_string();
-                position += 1;
 
-                while let Some(next_char) = source_code.chars().nth(position) {
+                while let Some(next_char) = source_code.chars().nth(position + 1) {
                     match next_char {
                         _n if next_char.is_digit(10) || next_char == '.' => {
-                            number_lexeme.push(next_char);
                             position += 1;
-                        },
+                            number_lexeme.push(next_char);
+                        }
                         ' ' | '\n' | ')' | ';' | '+' | '-' | '*' | '/' | '%' | '=' => break,
-                        _ => return Err(SyntaxError::InvalidNumber {
-                            line: 9999,
-                            at: format!("{}{}", number_lexeme, next_char),
-                        }.to_string())
+                        _ => {
+                            return Err(SyntaxError::InvalidNumber {
+                                line: 9999,
+                                at: format!("{}{}", number_lexeme, next_char),
+                            }
+                            .to_string())
+                        }
                     }
                 }
 
@@ -87,64 +91,60 @@ pub fn tokenize(source_code: &str) -> Result<Vec<Token>, String> {
                     return Err(SyntaxError::InvalidNumber {
                         line: 9999,
                         at: number_lexeme.clone(),
-                    }.to_string());
+                    }
+                    .to_string());
                 }
-
                 tokens.push(token(&number_lexeme, TokenType::Number));
-            },
+            }
             '#' => {
                 let mut comment_lexeme = character.to_string();
+                position += 1;
 
-                if source_code.chars().nth(position + 1).is_some()
-                    && source_code.chars().nth(position + 2).is_some()
-                    && source_code.as_bytes()[position + 1] as char == '#'
-                    && source_code.as_bytes()[position + 2] as char == '#'
+                if source_code.as_bytes().get(position) == Some(&(b'#' as u8))
+                    && source_code.as_bytes().get(position + 1) == Some(&(b'#' as u8))
                 // if "###" then block comment
                 {
-                    position += 1;
+                    position += 2;
+                    comment_lexeme.push_str("##");
+                    //jumping next chars and adding the ## cause we know the next two chars are #
 
                     while position < source_code.len() {
                         comment_lexeme.push(source_code.as_bytes()[position] as char);
 
-                        if source_code.len() - position > 3
-                            && source_code.as_bytes()[position] as char == '#'
-                            && source_code.as_bytes()[position + 1] as char == '#'
-                            && source_code.as_bytes()[position + 2] as char == '#'
-                        {
-                            comment_lexeme.push_str("##");
-                            position += 2; //to get past the two # 
+                        if comment_lexeme.ends_with("###") {
+                            position += 2; //to get past the two #
                             break;
                         }
 
                         position += 1;
                     }
+
                     tokens.push(token(&comment_lexeme, TokenType::BlockComment));
                 } else {
-                    position += 1;
                     comment_lexeme.push(source_code.as_bytes()[position] as char);
 
                     while position < source_code.len() {
-                        let next_char = source_code.as_bytes()[position + 1] as char;
-                        comment_lexeme.push(next_char);
+                        comment_lexeme.push(source_code.as_bytes()[position + 1] as char);
                         position += 1;
 
-                        if next_char == '#' {
+                        if comment_lexeme.ends_with('#') {
                             return Err(SyntaxError::Comment {
                                 line: 000,
-                                message: format!("Cannot start a comment in another comment: '{comment_lexeme}'"),
+                                message: format!(
+                                    "Cannot start a comment in another comment: '{comment_lexeme}'"
+                                ),
                             }
                             .to_string());
                         }
-                        
-                        if next_char == '\n' {
+
+                        if comment_lexeme.ends_with('\n') {
                             break;
                         }
                     }
 
                     tokens.push(token(&comment_lexeme, TokenType::LineComment));
                 }
-                println!("position: {position}")
-            }
+            },
             _ => (),
         };
         position += 1;
