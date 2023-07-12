a5b0b2a09d39c271f5522a8602f63aadda6ee2c3
IronYatsu <yatsu.dev@gmail.com>
2023-07-13 01:32:16 +0200

    useless print removed

--- src/main.rs
+++ src/main.rs
@@ -12,14 +12,11 @@ const FILE_PATH: &str = "./tests/vars.jf";
 fn main() -> Result<(), Box<dyn Error>> {
     let contents: String = fs::read_to_string(FILE_PATH)?;
 
-    println!("{:?}", time(&contents, tokenize));
+    time(&contents, tokenize);
     Ok(())
 }
 
-fn time(
-    content: &str,
-    func: impl Fn(&str) -> Result<Vec<Token>, String>,
-) -> Vec<Token> {
+fn time(content: &str, func: impl Fn(&str) -> Result<Vec<Token>, String>) -> Vec<Token> {
     let start = Instant::now();
     let result = func(content);
     let end = Instant::now();
